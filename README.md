858a44b17ccd4d6188432dd27a1c36ef2473981f
mozayed007 <s-mohamedzayed@zewailcity.edu.eg>
2023-01-26 16:46:38 +0200

    First tool/Script

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Images  PDF Converter (Manga Version)
- A tool script that converts images in sub-directories to one single PDF in the parent directory
+
+A tool script that converts images in sub-directories to one single PDF in the parent directory.
+
+## Usage
+
+The script takes in two arguments:
+
+- `folder`: The path to the manga folder that contains the images.
+- `-o, --output`: (optional) The name of the output pdf file. If not provided, the pdf file will be named after the manga folder.
+
+## Functionality
+
+1. The script first initializes an argument parser to handle the input arguments.
+2. The script then initializes a pdf merger from the PyPDF2 library.
+3. The script then changes the current working directory to the parent folder specified in the input argument.
+4. The script then iterates through the subfolders in the parent folder, and for each subfolder, it converts all jpg and JPG images to pdf.
+5. After all the images have been converted, the script iterates through the files in the parent folder and appends all pdf files to the pdf merger.
+6. The script then writes the merged pdf to the parent folder with the specified output name or the name of the parent folder if no output name is provided.
+7. The script then closes the pdf merger and deletes the individual chapter pdfs.
+
+## Requirements
+
+- Python 3
+- PyPDF2
+- PIL
+- img2pdf
+- send2trash
+
+## Note
+
+Make sure to install the required packages before running the script. Use command `pip install -r requirements.txt` to install all the packages at once if you have a requirements.txt file.
---
+++ Script.py
@@ -0,0 +1,69 @@
+import argparse
+from PyPDF2 import PdfFileMerger
+import os
+import re
+from PIL import Image
+import img2pdf
+from send2trash import send2trash
+
+# Initialize the arguement parser
+parser = argparse.ArgumentParser(description='Merge and convert manga images to pdf')
+
+# Add the folder path arguement
+parser.add_argument('folder', type=str, help='Path to the manga folder')
+
+# Add the output pdf file name arguement
+parser.add_argument('-o', '--output', type=str, help='Name of the output pdf file')
+
+# Parse the arguements
+args = parser.parse_args()
+
+# Initialize the pdf merger
+merger = PdfFileMerger()
+
+# Get the parent folder path
+parent_folder = args.folder
+
+# Change the current working directory to the parent folder
+os.chdir(parent_folder)
+
+
+# Iterate through the subfolders in the parent folder
+for subfolder in os.listdir(parent_folder):
+    if os.path.isdir(subfolder):
+        for filename in os.listdir(subfolder):
+            if filename.endswith(('.jpg', '.JPG')):
+                filename_regex = re.compile(r'(\.jpg)|(\.jpeg)', re.IGNORECASE)
+                new_name = filename_regex.sub('', filename)
+                image = Image.open(f"{subfolder}/{filename}")
+                pdf_bytes = img2pdf.convert(image.filename)
+                pdf_path = f"{subfolder}/{new_name}.pdf"
+                with open(pdf_path, 'wb') as pdf_file:
+                    pdf_file.write(pdf_bytes)
+                print(f"Converted {filename} to {pdf_path}")
+                
+                
+# Iterate through the files in the parent folder
+for file in os.listdir(parent_folder ): 
+    if file.endswith('.pdf'):
+        print(f"Merging file {file}")
+        merger.append(file)   
+        
+# Get the name of the parent folder
+manga = os.path.basename(os.path.normpath(parent_folder))
+
+# Write the merged PDF to the parent folder
+if args.output:
+    output_path = f"{parent_folder}/{args.output}.pdf"
+else:
+    output_path = f"{parent_folder}/{manga}.pdf"
+
+merger.write(output_path)
+
+# Close the PDF merger
+merger.close()
+
+# Iterate through the files in the parent folder
+for file in os.listdir(parent_folder ): 
+    if file.startswith('Ch.') and  file.endswith('.pdf'): 
+        print(f"Deleting file {file}")
